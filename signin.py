79116507b1abe66fd4bd4e639f368b649d045866
Alhaj53 <eimamalhaj@gmail.com>
2026-06-21 17:50:19 +0000

    Update signin.py

--- signin.py
+++ signin.py
@@ -472,8 +472,8 @@ async def get_balance():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 if __name__ == "__main__":
+    import os
     app.run(
         host="0.0.0.0",
-        port=5000,
-        debug=False
-        )
+        port=int(os.environ.get("PORT", 10000))
+    )
